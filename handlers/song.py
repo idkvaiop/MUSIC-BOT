18c0076d0fd5f0f80751466a78e5dbe2092fe095
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-09-11 19:08:40 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "Your Search Does Not Match Results...!!!"
+            "Your Query Does Not Match Any Results...!!!"
         ) 
         print(str(e))
         return
-    m.edit("Download Song")
+    m.edit("Fetching Song...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**Maintained by :-** [Akhil 🇮🇳](tg://user?id={2102783671}) **\n**Owned by :-** [𓆩𝙇𝙊𝙍𝘿 𝙇𝙐𝘾𝙄𝙁𝙀𝙍𓆪](tg://user?id={5082902573})"
+        rep = "☆━━━━━━━━━━━━━━━━☆\n**Maintained by :-** [Akhil 🇮🇳](tg://user?id={2102783671}) **\n**Owned by :-** [𓆩𝙇𝙊𝙍𝘿 𝙇𝙐𝘾𝙄𝙁𝙀𝙍𓆪](tg://user?id={5082902573})"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
